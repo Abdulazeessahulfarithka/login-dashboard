332252733dfa1cfa8376a08578fbd4c789e378c1
“Abdul <kaazees31@gmail.com>
2024-06-30 09:06:10 +0530

    Fixed build issues and updated dependencies

--- src/Components/card.js
+++ src/Components/card.js
@@ -3,7 +3,7 @@ import React from 'react'
 function card() {
   return (
     <select class="form-select" aria-label="Default select example">
-    <option selected> menu</option>
+    <option selected>Create</option>
     <option value="1">Create Organisation</option>
     <option value="2">Create Admin</option>
   </select>
